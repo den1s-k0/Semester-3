1a239499c202cce19cd2483c57ea14108e0d13ec
Korol Denis <koroldenis0904@gmail.com>
2025-09-25 09:35:14 +0300

    Add files via upload

--- OS(Operating System)/Lab OS 01 KD/main.cpp
+++ OS(Operating System)/Lab OS 01 KD/main.cpp
@@ -1,9 +1,5 @@
-#include <windows.h>
-#include <conio.h>
-#include <iostream>
-#include <string>
-
-using namespace std;
+﻿#include <conio.h>
+#include "header.h"
 
 int main()
 {
@@ -12,53 +8,23 @@ int main()
 	int employeeNumber, hourly_rate;
 	cout << "\nEnter bin-file name: ";
 	cin >> BinName;
+	BinName += ".bin";
 	cout << "\nEnter number of employees: ";
 	cin >> employeeNumber;
 
-	BinAddress = "Creator.exe " + BinName + ".bin " + to_string(employeeNumber);
-
-	STARTUPINFO siCreator;
-	PROCESS_INFORMATION piCreator;
-	ZeroMemory(&siCreator, sizeof(STARTUPINFO));
-	siCreator.cb = sizeof(STARTUPINFO);
-	if (!CreateProcessA(NULL, (LPSTR)BinAddress.c_str(), NULL, NULL, FALSE,
-		0, NULL, NULL, &siCreator, &piCreator))
-	{
-		_cputs("The new process is not created.\n");
-		_cputs("Check a name of the process.\n");
-		_cputs("Press any key to finish.\n");
-		_getch();
-		return 0;
-	}
-	_cputs("The new process is created.\n");
-	WaitForSingleObject(piCreator.hProcess, INFINITE);
-	CloseHandle(piCreator.hThread);
-	CloseHandle(piCreator.hProcess);
+	BinAddress = "Creator.exe " + BinName + " " + to_string(employeeNumber);
+	startProcess(BinAddress);
+	readBinFile(BinName);
 
 	cout << "\nEnter report-file name: ";
 	cin >> ReportName;
+	ReportName += ".txt";
 	cout << "\nEnter hourly rate: ";
 	cin >> hourly_rate;
 
-	ReportAddress = "Reporter.exe " + BinName + ".bin " + ReportName + ".txt " + to_string(hourly_rate);
-
-	STARTUPINFO siReporter;
-	PROCESS_INFORMATION piReporter;
-	ZeroMemory(&siReporter, sizeof(STARTUPINFO));
-	siReporter.cb = sizeof(STARTUPINFO);
-	if (!CreateProcess(NULL, (LPSTR)ReportAddress.c_str(), NULL, NULL, FALSE,
-		0, NULL, NULL, &siReporter, &piReporter))
-	{
-		_cputs("The new process is not created.\n");
-		_cputs("Check a name of the process.\n");
-		_cputs("Press any key to finish.\n");
-		_getch();
-		return 0;
-	}
-	_cputs("The new process is created.\n");
-	WaitForSingleObject(piReporter.hProcess, INFINITE);
-	CloseHandle(piReporter.hThread);
-	CloseHandle(piReporter.hProcess);
+	ReportAddress = "Reporter.exe " + BinName + " " + ReportName + " " + to_string(hourly_rate);
+	startProcess(ReportAddress);
+	readTextFile(ReportName);
 
 	return 0;
 }

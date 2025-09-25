97e17d257429f45de40ef32e446065b76c4f79ef
Korol Denis <koroldenis0904@gmail.com>
2025-09-25 09:32:26 +0300

    Update creator.cpp

--- OS(Operating System)/Lab OS 01 KD/creator.cpp
+++ OS(Operating System)/Lab OS 01 KD/creator.cpp
@@ -5,7 +5,7 @@ int main(int argc, char* argv[]) {
 	ofstream out(argv[1], ios::binary);
 	if (!out.is_open()) {
 		cout << "Error\nBinFile isn't create";
-		return 0;
+		return 1;
 	}
 	employee tipok;
 	for (int i = 0; i < stoi(argv[2]); i++) {

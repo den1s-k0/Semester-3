e31b1af53d3ae1de62739e0019bcaa06fe2c8a1d
Korol Denis <koroldenis0904@gmail.com>
2025-09-25 10:47:07 +0300

    Add files via upload

--- ПП(Промышленное программирование)/Lab IP 01-6 KD/Main.java
+++ ПП(Промышленное программирование)/Lab IP 01-6 KD/Main.java
@@ -1,6 +1,14 @@
+import java.io.InputStreamReader;
+import java.io.IOException;
 import java.util.Scanner;
 import java.util.Formatter;
+import java.util.Dictionary;
 import static java.lang.Math.sinh;
+import java.math.BigInteger;
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.io.BufferedReader;
+
 
 public class Main {
     public static void main(String[] args) {
@@ -28,8 +36,10 @@ class Tailor {
         int k = in.nextInt();
         double e = CalculateE(k);
         double res = CalculateTailor.CalculateSum(x, e);
-        fmt.format("Dec x: %d \nHex x: %x \nOctal x: %o \n", x, x, x);
+        fmt.format("Dec x:\t % d \n Hex x:\t %x \n Octal x:\t %#o \n", x, x, x);
         System.out.print(fmt);
+        System.out.printf("-: %-5d\n", x, 40);
+        System.out.printf("+: %+d\n", +x);
         System.out.printf("Result Tailor: %.12e \n", res);
         System.out.println("Result Built-in: " + sinh(x));
 
@@ -47,6 +57,65 @@ class CalculateTailor extends Tailor{
         for(int i = 2; component > e; i++) {
             sum += component;
             component *= (double) (x * x) / ((2 * i - 1) * (2 * i - 2));
+        }
+        return sum;
+    }
+}
+
+class BigTailor{
+    public static BigDecimal CalculateBigE(BigInteger k){
+        int scale = k.intValue() + 50;
+        MathContext mc = new MathContext(scale);
+
+        BigDecimal E = BigDecimal.valueOf(1);
+        BigDecimal T = BigDecimal.valueOf(10);
+        BigInteger O = BigInteger.valueOf(1);
+        BigInteger i = BigInteger.valueOf(0);
+
+        while(i.compareTo(k) < 0){
+            E = E.divide(T, mc);
+            i = i.add(O);
+        }
+
+        return E;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        BigDecimal e = BigDecimal.valueOf(0);
+
+        System.out.print("Enter x: ");
+        String input;
+        input = in.readLine();
+        BigDecimal x = new BigDecimal(input);
+        System.out.print("Enter k: ");
+        input = in.readLine();
+        BigInteger k = new BigInteger(input);
+        e = CalculateBigE(k);
+        BigDecimal res = CalculateBigTailor.CalculateBigSum(x, e, k);
+        System.out.printf("Result Tailor: " + res + "\n");
+        System.out.println("Result Built-in: " + sinh(x.doubleValue()));
+
+        in.close();
+    }
+}
+
+class CalculateBigTailor extends BigTailor{
+    public static BigDecimal CalculateBigSum(BigDecimal x, BigDecimal e, BigInteger k){
+        BigDecimal sum = BigDecimal.valueOf(0);
+        BigDecimal component = x;
+        int scale = k.intValue() + 50;
+        MathContext mc = new MathContext(scale);
+        //component *= (double) (x * x) / ((2 * i - 1) * (2 * i - 2));
+
+        BigDecimal i = BigDecimal.valueOf(2);
+
+        while(component.compareTo(e) > 0){
+            sum = sum.add(component);
+            component = component.multiply(x.multiply(x));
+            component = (component.divide((i.multiply(BigDecimal.valueOf(2))).subtract(BigDecimal.valueOf(1)), mc))
+                    .divide((i.multiply(BigDecimal.valueOf(2))).subtract(BigDecimal.valueOf(2)), mc);
+            i = i.add(BigDecimal.valueOf(1));
         }
         return sum;
     }

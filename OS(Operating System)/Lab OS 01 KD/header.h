b3fc1b0024eddf11846ef0567ee0dedda760d18a
Korol Denis <koroldenis0904@gmail.com>
2025-09-30 00:07:48 +0300

    Add files via upload

--- OS(Operating System)/Lab OS 01 KD/header.h
+++ OS(Operating System)/Lab OS 01 KD/header.h
@@ -1,9 +1,10 @@
-#pragma once
+﻿#pragma once
 
 #include <iostream>
 #include <windows.h>
 #include <fstream>
 #include <string>
+#include <cstring>
 
 using namespace std;
 
@@ -11,6 +12,15 @@ struct employee {
 	int num; // ID
 	char name[10]; // employee name
 	double hours; // number of hours
+
+    employee() : num(0), hours(0.0) {
+        strcpy(name, "");
+    }
+    employee(int n, const char* nm, double h) : num(n), hours(h) {
+        strncpy(name, nm, sizeof(name) - 1);
+        name[sizeof(name) - 1] = '\0';
+    }
+    ~employee() {}
 };
 ostream& operator <<(ostream& out, employee& a);
 istream& operator >>(istream& in, employee& a);
---
+++ OS(Operating System)/Lab OS 01 KD/tests/test_employee.cpp
@@ -0,0 +1,39 @@
+#include <gtest/gtest.h>
+#include <tuple>
+#include "../header.h"
+
+class EmployeeConstructorTest : public ::testing::TestWithParam<std::tuple<int, const char*, double>> {};
+
+// Набор тестовых данных
+INSTANTIATE_TEST_SUITE_P(
+    VariousInputs,
+    EmployeeConstructorTest,
+    ::testing::Values(
+        make_tuple(1, "Denis", 10.5),
+        make_tuple(2, "Anna", 0.0),
+        make_tuple(3, "Max", 99.9),
+        make_tuple(4, "", 5.0),
+        make_tuple(5, "LongName", 12.3)
+    )
+);
+
+TEST_P(EmployeeConstructorTest, InitializesCorrectly) {
+    int expected_num = get<0>(GetParam());
+    const char* expected_name = get<1>(GetParam());
+    double expected_hours = get<2>(GetParam());
+
+    employee e(expected_num, expected_name, expected_hours);
+
+    EXPECT_EQ(e.num, expected_num);
+    EXPECT_EQ(e.hours, expected_hours);
+    EXPECT_STREQ(e.name, expected_name);
+}
+
+
+// Тест конструктора по умолчанию
+TEST(EmployeeTest, DefaultConstructor) {
+    employee e;
+    EXPECT_EQ(e.num, 0);
+    EXPECT_EQ(e.hours, 0.0);
+    EXPECT_STREQ(e.name, "");
+}

8a5e795b925e99a8719555c81667dc13ffef3503
den1s-k0 <koroldenis0904@gmail.com>
2025-10-25 13:04:10 +0300

    First step of OS lab 3

---
+++ OS(Operating System)/Lab OS 03 KD/core.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+namespace ReturnCoreCodes {
+	const int SUCCES = 0;
+}
+
+vector <int> createVectorOfZeros();
+
+int cinThreadCount();
---
+++ OS(Operating System)/Lab OS 03 KD/main.cpp
@@ -0,0 +1,29 @@
+﻿#include "main.h"
+
+int main()
+{
+	vector<int> mas = createVectorOfZeros();
+	int thread_count = cinThreadCount();
+	HANDLE* hThread = new HANDLE[thread_count];
+	DWORD* dwThread = new DWORD[thread_count];
+
+	for (int i = 0; i < thread_count; i++) {
+		hThread[i] = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)thread,
+			NULL, 0, &dwThread[i]);
+		if (hThread[i] == NULL)
+			return GetLastError();
+	}
+
+	if (WaitForMultipleObjects(thread_count, hThread, TRUE, INFINITE) == WAIT_FAILED)
+	{
+		cout << "Wait for multiple objects failed." << endl;
+		cout << "Press any key to exit." << endl;
+	}
+	
+	for (int i = 0; i < thread_count; i++) {
+		CloseHandle(hThread[i]);
+	}
+
+
+	return 0;
+}

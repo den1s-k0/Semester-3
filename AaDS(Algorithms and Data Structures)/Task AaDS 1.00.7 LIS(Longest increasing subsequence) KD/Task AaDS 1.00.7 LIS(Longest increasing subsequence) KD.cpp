4bebecc81553d561f229c518a32323aed97eb35a
Korol Denis <koroldenis0904@gmail.com>
2025-11-03 23:00:57 +0300

    Add files via upload

---
+++ AaDS(Algorithms and Data Structures)/Task AaDS 1.00.7 LIS(Longest increasing subsequence) KD/Task AaDS 1.00.7 LIS(Longest increasing subsequence) KD.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+int LowerBound(vector<int>& mas, int x) {
+	//if (mas.empty()) return 0;
+
+	int left = 0, right = mas.size(), k;
+	while (left < right) {
+		k = (left + right) / 2;
+		if (x <= mas[k])
+			right = k;
+		else
+			left = k + 1;
+	}
+	return left;
+}
+
+int main()
+{
+	ifstream in("input.txt");
+	if (!in.is_open())
+		return 1;
+	int size;
+	in >> size;
+	vector<int> result;
+	vector<int> input(size);
+
+	for (int i = 0; i < size; i++) {
+		in >> input[i];
+		int ind = LowerBound(result, input[i]);
+		if (ind == result.size())
+			result.push_back(input[i]);
+		else
+			result[ind] = input[i];
+	}
+	in.close();
+
+
+	ofstream out("output.txt");
+	if (!out.is_open()) {
+		return 1;
+	}
+	out << result.size();
+	out.close();
+
+	return 0;
+}

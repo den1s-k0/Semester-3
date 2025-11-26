d2d5cdec4fe4fdb0360ff3fcc9e91d3d57d6a903
Korol Denis <koroldenis0904@gmail.com>
2025-11-27 01:01:41 +0300

    Add files via upload

---
+++ IP(Industrial Programming)/Lab IP 03-6 KD/CafeMenu/MainCourse.java
@@ -0,0 +1,55 @@
+package CafeMenu;
+
+public class MainCourse extends CafeMenuPosition{
+
+    public int getMass() {
+        return mass;
+    }
+
+    public boolean isVegetarian() {
+        return vegetarian;
+    }
+
+    public void setMass(int mass) {
+        this.mass = mass;
+    }
+
+    public void setVegetarian(boolean vegetarian) {
+        this.vegetarian = vegetarian;
+    }
+
+    private int mass;
+    private boolean vegetarian;
+
+    public MainCourse(int ID, String NAME, double PRICE, String CATEGORY, double CALORIES, int MASS, boolean VEGETARIAN) {
+        super(ID, NAME, PRICE, CATEGORY, CALORIES);
+        this.mass = MASS;
+        this.vegetarian = VEGETARIAN;
+    }
+
+    @Override
+    public String toString(){
+        return String.format(
+                super.toString() + "Масса: %d мл.\n", mass,
+                isVegetarian() ? "Вегетарианский" : "Не вегетарианский"
+        );
+    }
+
+    public void getInfo(){
+        System.out.println("\"ID: " + getId());
+        System.out.println("Название: " + getName());
+        System.out.println("Категория: " +  getCategory());
+        System.out.println(isVegetarian() ? "Вегетарианский" : "Не вегетарианский");
+        System.out.println("Цена: " + getPrice());
+        System.out.println("Масса" + getMass());
+        System.out.println("Калории: " + getCalories());
+        System.out.println("Дата добавления: " + getAddedDate());
+    }
+
+    @Override
+    String getDishType() {
+        return (isVegetarian() ? "Вегетарианское" : "Не вегетарианское" + " блюдо ");
+    }
+
+}
+
---
+++ IP(Industrial Programming)/Lab IP 03-6 KD/rolls/Rolls.java
@@ -0,0 +1,29 @@
+package rolls;
+
+import CafeMenu.CafeMenuPosition;
+
+import java.util.Comparator;
+import java.util.List;
+
+public abstract class Rolls<T extends CafeMenuPosition> {
+    public abstract void AddElement(T elem);
+
+    public abstract boolean DeleteElement(T elem);
+
+    public abstract void ReplaceElement(T elem);
+
+    protected abstract boolean ContainID(T elem);
+
+    public abstract T GetElement(int ID, String CATEGORY);
+
+    public abstract List<T> GetAllElements();
+
+    public abstract int GetSize();
+
+    public abstract void Clear();
+
+    public abstract void PrintList();
+
+    public abstract void sort(Comparator<T> comparator);
+}
+

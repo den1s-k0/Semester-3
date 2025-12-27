ac6b33ff56b1f9f51f4004505ec9e05198c1cc0e
den1s-k0 <koroldenis0904@gmail.com>
2025-12-27 12:24:49 +0300

    Add files via upload

--- OS(Operating System)/Lab OS 05 KD/client.cpp
+++ OS(Operating System)/Lab OS 05 KD/client.cpp
@@ -1,4 +1,13 @@
 #include "client.h"
+#include <cstring>
+
+bool isPipeConnected(HANDLE hPipe) {
+    DWORD bytesAvailable = 0;
+    if (!PeekNamedPipe(hPipe, NULL, 0, NULL, &bytesAvailable, NULL)) {
+        return false;
+    }
+    return true;
+}
 
 int main(int argc, char* argv[]) {
     if (argc < 3) {
@@ -15,6 +24,11 @@ int main(int argc, char* argv[]) {
     }
 
     while (true) {
+        if (!isPipeConnected(hPipe)) {
+            cout << "Server disconnected. Exiting..." << endl;
+            break;
+        }
+
         cout << endl << "1. Modify record" << endl;
         cout << "2. Read record" << endl;
         cout << "3. Exit" << endl;
@@ -28,7 +42,9 @@ int main(int argc, char* argv[]) {
         if (choice == ClientConstants::EXIT_CHOICE) {
             int operation = OperationType::END_OPERATION;
             DWORD bytesWritten;
-            WriteFile(hPipe, &operation, sizeof(operation), &bytesWritten, NULL);
+            if (!WriteFile(hPipe, &operation, sizeof(operation), &bytesWritten, NULL)) {
+                cout << "Server disconnected during exit" << endl;
+            }
             break;
         }
 
@@ -42,22 +58,42 @@ int main(int argc, char* argv[]) {
 
         if (choice == ClientConstants::READ_CHOICE) {
             int operation = OperationType::READ_OPERATION;
-            WriteFile(hPipe, &operation, sizeof(operation), &bytesWritten, NULL);
-            WriteFile(hPipe, &recordIndex, sizeof(recordIndex), &bytesWritten, NULL);
+            if (!WriteFile(hPipe, &operation, sizeof(operation), &bytesWritten, NULL)) {
+                cout << "Server disconnected" << endl;
+                break;
+            }
+
+            if (!WriteFile(hPipe, &recordIndex, sizeof(recordIndex), &bytesWritten, NULL)) {
+                cout << "Server disconnected" << endl;
+                break;
+            }
 
             Employee emp;
-            ReadFile(hPipe, &emp, sizeof(emp), &bytesRead, NULL);
+            if (!ReadFile(hPipe, &emp, sizeof(emp), &bytesRead, NULL)) {
+                cout << "Server disconnected while reading response" << endl;
+                break;
+            }
 
             cout << endl << "Record contents:" << endl;
             cout << emp;
         }
         else if (choice == ClientConstants::MODIFY_CHOICE) {
             int operation = OperationType::READ_OPERATION;
-            WriteFile(hPipe, &operation, sizeof(operation), &bytesWritten, NULL);
-            WriteFile(hPipe, &recordIndex, sizeof(recordIndex), &bytesWritten, NULL);
+            if (!WriteFile(hPipe, &operation, sizeof(operation), &bytesWritten, NULL)) {
+                cout << "Server disconnected" << endl;
+                break;
+            }
+
+            if (!WriteFile(hPipe, &recordIndex, sizeof(recordIndex), &bytesWritten, NULL)) {
+                cout << "Server disconnected" << endl;
+                break;
+            }
 
             Employee emp;
-            ReadFile(hPipe, &emp, sizeof(emp), &bytesRead, NULL);
+            if (!ReadFile(hPipe, &emp, sizeof(emp), &bytesRead, NULL)) {
+                cout << "Server disconnected while reading current record" << endl;
+                break;
+            }
 
             cout << endl << "Current record:" << endl;
             cout << emp;
@@ -78,12 +114,26 @@ int main(int argc, char* argv[]) {
             if (input != "-") emp.hours = stod(input);
 
             operation = OperationType::WRITE_OPERATION;
-            WriteFile(hPipe, &operation, sizeof(operation), &bytesWritten, NULL);
-            WriteFile(hPipe, &recordIndex, sizeof(recordIndex), &bytesWritten, NULL);
-            WriteFile(hPipe, &emp, sizeof(emp), &bytesWritten, NULL);
+            if (!WriteFile(hPipe, &operation, sizeof(operation), &bytesWritten, NULL)) {
+                cout << "Server disconnected" << endl;
+                break;
+            }
+
+            if (!WriteFile(hPipe, &recordIndex, sizeof(recordIndex), &bytesWritten, NULL)) {
+                cout << "Server disconnected" << endl;
+                break;
+            }
+
+            if (!WriteFile(hPipe, &emp, sizeof(emp), &bytesWritten, NULL)) {
+                cout << "Server disconnected" << endl;
+                break;
+            }
 
             bool success;
-            ReadFile(hPipe, &success, sizeof(success), &bytesRead, NULL);
+            if (!ReadFile(hPipe, &success, sizeof(success), &bytesRead, NULL)) {
+                cout << "Server disconnected while waiting for confirmation" << endl;
+                break;
+            }
 
             if (success) {
                 cout << "Record updated successfully" << endl;
@@ -95,8 +145,4 @@ int main(int argc, char* argv[]) {
     cout << "Client finished" << endl;
 
     return 0;
-}
-
-int clientMain(int argc, char* argv[]) {
-    return main(argc, argv);
 }
